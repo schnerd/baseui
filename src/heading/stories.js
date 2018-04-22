c5362228dea76e68a3cdc274de26400fdbc35d70
Diana Suvorova <diana.suvorova@uber.com>
2018-04-22 10:46:16 -0600

    feat: stories with knobs

--- src/heading/stories.js
+++ src/heading/stories.js
@@ -1,12 +1,24 @@
 // @flow
 import React from 'react';
-
 import {storiesOf} from '@storybook/react';
-
+import {withKnobs, number, text, boolean} from '@storybook/addon-knobs/react';
 import Heading from './Heading';
 
-storiesOf('Heading', module)
-  .add('default', () => <Heading>Hello</Heading>)
-  .add('smaller Heading', () => <Heading size={2}>Hello</Heading>)
+const stories = storiesOf('Heading', module);
+stories.addDecorator(withKnobs);
+
+stories
+  .add('Default', () => <Heading>Default</Heading>)
+  .add('With Knobs', () => (
+    <Heading
+      size={number('size', 2)}
+      color={text('color', 'darkGray')}
+      overflow={text('overflow', 'breakWord')}
+      truncate={boolean('truncate', false)}
+    >
+      {text('label', 'Heading')}
+    </Heading>
+  ))
+  .add('Smaller', () => <Heading size={number('size', 2)}>Smaller</Heading>)
   .add('larger Heading', () => <Heading size={4}>Hello</Heading>)
   .add('Blue heading', () => <Heading color="darkBlue">Hello</Heading>);
