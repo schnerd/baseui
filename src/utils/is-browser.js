dc99f4d620d6adba4b6d9933bb407337e5098a8d
Diana Suvorova <diana.suvorova@uber.com>
2018-07-11 13:37:49 -0700

    wip: proper merge

--- src/utils/is-browser.js
+++ src/utils/is-browser.js
@@ -1,9 +1,5 @@
 // @flow
-<<<<<<< Updated upstream
-import {document} from 'global';
-=======
 import document from 'global/document';
->>>>>>> Stashed changes
 
 export default typeof document !== 'undefined' &&
   Boolean(document.createElement);
