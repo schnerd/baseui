411e18f7baf806b0c581063433d139292051f587
David Schnurr <dschnurr@uber.com>
2018-08-03 13:44:28 -0700

    experiment with component specific theme overrides for input

--- src/components/input/styled-components.js
+++ src/components/input/styled-components.js
@@ -2,24 +2,25 @@
 import {styled} from '../../styles';
 import {ADJOINED, SIZE, ENHANCER_POSITION} from './constants';
 
-function getInputPadding(size, sizing) {
+function getInputPadding(size, theme) {
   return {
     [SIZE.default]: {
-      paddingTop: sizing.scale400,
-      paddingRight: sizing.scale500,
-      paddingBottom: sizing.scale400,
-      paddingLeft: sizing.scale500,
+      paddingTop: theme.inputDefaultSizePaddingTop || theme.sizing.scale400,
+      paddingRight: theme.inputDefaultSizePaddingRight || theme.sizing.scale500,
+      paddingBottom: theme.inputDefaultSizePaddingRight || theme.sizing.scale400,
+      paddingLeft: theme.inputDefaultSizePaddingRight || theme.sizing.scale500,
     },
     [SIZE.compact]: {
-      paddingTop: sizing.scale200,
-      paddingRight: sizing.scale500,
-      paddingBottom: sizing.scale200,
-      paddingLeft: sizing.scale500,
+      paddingTop: theme.inputDefaultSizePaddingRight || theme.sizing.scale200,
+      paddingRight: theme.inputDefaultSizePaddingRight || theme.sizing.scale500,
+      paddingBottom: theme.inputDefaultSizePaddingRight || theme.sizing.scale200,
+      paddingLeft: theme.inputDefaultSizePaddingRight || theme.sizing.scale500,
     },
   }[size];
 }
 
-function getBorderRadius(adjoined, radius) {
+function getBorderRadius(adjoined, theme) {
+  const radius = theme.inputBorderRadius || theme.sizing.scale100;
   return {
     [ADJOINED.none]: radius,
     [ADJOINED.left]: `0 ${radius} ${radius} 0`,
@@ -28,76 +29,79 @@ function getBorderRadius(adjoined, radius) {
   }[adjoined];
 }
 
-function getDecoratorBorderRadius(position, radius) {
+function getDecoratorBorderRadius(position, theme) {
+  const radius = theme.inputDecoraterBorderRadius || theme.sizing.scale100;
   return {
     [ENHANCER_POSITION.start]: `${radius} 0 0 ${radius}`,
     [ENHANCER_POSITION.end]: `0 ${radius} ${radius} 0`,
   }[position];
 }
 
-function getFont(size, typography) {
+function getFont(size, theme) {
   return {
-    [SIZE.default]: typography.font300,
-    [SIZE.compact]: typography.font200,
+    [SIZE.default]: theme.inputDefaultSizeFont || theme.typography.font300,
+    [SIZE.compact]: theme.inputCompactSizeFont || theme.typography.font200,
   }[size];
 }
 
 export const Root = styled('div', props => {
-  const {$size, $theme: {colors, typography}} = props;
+  const {$size, $theme} = props;
   return {
-    ...getFont($size, typography),
-    color: colors.mono1000,
+    ...getFont($size, $theme),
+    color: $theme.inputTextColor || $theme.colors.mono1000,
     display: 'flex',
     width: '100%',
   };
 });
 
 export const Label = styled('label', props => {
-  const {$disabled, $theme: {colors, sizing, typography}} = props;
+  const {$disabled, $theme} = props;
   return {
-    ...typography.font350,
+    ...($theme.inputLabelFont || $theme.typography.font350),
     fontWeight: 500,
-    color: $disabled ? colors.mono700 : colors.mono1000,
+    color: $disabled 
+      ? $theme.inputLabelDisabledColor || $theme.colors.mono700 
+      : $theme.inputLabelColor || $theme.colors.mono1000,
     display: 'block',
     paddingTop: '0',
     paddingRight: '0',
     paddingBottom: '0',
     paddingLeft: '0',
-    marginTop: sizing.scale300,
-    marginRight: '0',
-    marginBottom: sizing.scale300,
-    marginLeft: '0',
+    marginTop: $theme.inputLabelMarginTop || $theme.sizing.scale300,
+    marginRight: $theme.inputLabelMarginRight || '0',
+    marginBottom: $theme.inputLabelMarginBottom || $theme.sizing.scale300,
+    marginLeft: $theme.inputLabelMarginLeft || '0',
   };
 });
 
 export const Caption = styled('div', props => {
-  const {$error, $theme: {colors, sizing, typography}} = props;
+  const {$error, $theme} = props;
   return {
-    ...typography.font200,
+    ...($theme.inputCaptionFont || $theme.typography.font200),
     color:
       $error && typeof $error !== 'boolean'
-        ? colors.negative400
-        : colors.mono800,
+        ? $theme.inputCaptionErrorTextColor || $theme.colors.negative400
+        : $theme.inputCaptionTextColor || $theme.colors.mono800,
     paddingTop: '0',
     paddingRight: '0',
     paddingBottom: '0',
     paddingLeft: '0',
-    marginTop: sizing.scale300,
-    marginRight: '0',
-    marginBottom: sizing.scale300,
-    marginLeft: '0',
+    marginTop: $theme.inputCaptionMarginTop || $theme.sizing.scale300,
+    marginRight: $theme.inputCaptionMarginRight || '0',
+    marginBottom: $theme.inputCaptionMarginBottom || $theme.sizing.scale300,
+    marginLeft: $theme.inputCaptionMarginLeft || '0',
   };
 });
 
 export const InputEnhancer = styled('div', props => {
-  const {$position, $size, $theme: {colors, sizing, typography}} = props;
+  const {$position, $size, $theme} = props;
   return {
-    ...getFont($size, typography),
-    color: colors.mono900,
+    ...getFont($size, $theme),
+    color: $theme.inputTextColor || $theme.colors.mono900,
     display: 'flex',
-    ...getInputPadding($size, sizing),
-    backgroundColor: colors.mono400,
-    borderRadius: getDecoratorBorderRadius($position, sizing.scale100),
+    ...getInputPadding($size, $theme),
+    backgroundColor: $theme.inputEnhancerBackgroundColor || $theme.colors.mono400,
+    borderRadius: getDecoratorBorderRadius($position, $theme),
   };
 });
 
@@ -108,35 +112,43 @@ export const InputContainer = styled('div', props => {
     $error,
     $disabled,
     $size,
-    $theme: {colors, sizing, typography, animation},
+    $theme
   } = props;
   return {
-    ...getFont($size, typography),
-    color: $disabled ? colors.mono600 : colors.mono1000,
+    ...getFont($size, $theme),
+    color: $disabled
+      ? $theme.inputDisabledTextColor || $theme.colors.mono600
+      : $theme.inputTextColor || $theme.colors.mono1000,
     boxSizing: 'border-box',
     display: 'flex',
     width: '100%',
     backgroundColor: $disabled
-      ? colors.mono300
-      : $isFocused || $error ? colors.mono100 : colors.mono200,
+      ? $theme.inputDisabledBackgroundColor || $theme.colors.mono300
+      : $isFocused || $error
+        ? $theme.inputFocusedBackgroundColor || $theme.colors.mono100
+        : $theme.inputBackgroundColor || $theme.colors.mono200,
     borderWidth: '1px',
     borderStyle: 'solid',
     borderColor: $disabled
-      ? colors.mono300
+      ? $theme.inputDisabledBorderColor || $theme.colors.mono300
       : $error
-        ? colors.negative400
-        : $isFocused ? colors.primary400 : colors.mono200,
-    borderRadius: getBorderRadius($adjoined, sizing.scale100),
+        ? $theme.inputErrorBorderColor || $theme.colors.negative400
+        : $isFocused
+          ? $theme.inputFocusedBorderColor || $theme.colors.primary400
+          : $theme.inputBorderColor || $theme.colors.mono200,
+    borderRadius: getBorderRadius($adjoined, $theme),
     boxShadow: `0 2px 6px ${
       $disabled
         ? 'transparent'
         : $isFocused
-          ? $error ? colors.shadowError : colors.shadowFocus
+          ? $error
+            ? $theme.inputErrorShadow || $theme.colors.shadowError
+            : $theme.inputFocusShadow || $theme.colors.shadowFocus
           : 'transparent'
     }`,
     transitionProperty: 'border, boxShadow, backgroundColor',
-    transitionDuration: animation.timing100,
-    transitionTimingFunction: animation.easeOutCurve,
+    transitionDuration: $theme.animation.timing100,
+    transitionTimingFunction: $theme.animation.easeOutCurve,
   };
 });
 
@@ -145,21 +157,27 @@ export const Input = styled('input', props => {
     $disabled,
     $error,
     $size,
-    $theme: {colors, sizing, typography},
+    $theme,
   } = props;
   return {
-    ...getFont($size, typography),
-    color: $disabled ? colors.mono600 : colors.mono1000,
-    caretColor: $error ? colors.negative400 : colors.primary,
+    ...getFont($size, $theme),
+    color: $disabled
+      ? $theme.inputDisbaledTextColor || $theme.colors.mono600 
+      : $theme.inputTextColor || $theme.colors.mono1000,
+    caretColor: $error
+      ? $theme.inputErrorCaretColor || $theme.colors.negative400
+      : $theme.inputCaretColor || $theme.colors.primary,
     boxSizing: 'border-box',
     backgroundColor: 'transparent',
     borderWidth: '0',
     borderStyle: 'none',
     outline: 'none',
-    ...getInputPadding($size, sizing),
+    ...getInputPadding($size, $theme),
     width: '100%',
     '::placeholder': {
-      color: $disabled ? colors.mono600 : colors.mono700,
+      color: $disabled
+        ? $theme.inputPlaceholderDisabledTextColor || $theme.colors.mono600
+        : $theme.inputPlaceholderTextColor || $theme.colors.mono700,
     },
     ':hover': {
       cursor: $disabled ? 'not-allowed' : 'text',
